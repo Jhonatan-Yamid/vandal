5234d00b1092f7254b42c6d70fa8bd5bdba9d42e
Jhonatan Alzate <yamidjhonatan@gmail.com>
2026-08-07 02:23:46 -0500

    fix(cache):cache in config

--- src/app/api/configuracion/route.js
+++ src/app/api/configuracion/route.js
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { getConfiguracion } from "@/lib/config";
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   const config = await getConfiguracion();
